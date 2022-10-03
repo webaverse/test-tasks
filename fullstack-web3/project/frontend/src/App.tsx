28949305200983266e09f9a24f5d788e3905f68a
ivyroot <nonagoncup@gmail.com>
2022-10-03 15:36:06 -0700

    add React Query to wrap http calls

--- fullstack-web3/project/frontend/src/App.tsx
+++ fullstack-web3/project/frontend/src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <>
       <div className="flex">
-        <img src={logo} className="w-32" alt="logo" />
+        <img src={logo} className="w-20" alt="logo" />
       </div>
-      <p className='m-8'>
-        Coins Available RN
-      </p>
+      <QueryClientProvider client={queryClient}>
+        <p className='m-8'>
+          Coins Available RN
+        </p>
+      </QueryClientProvider>
     </>
   );
 }
