22e7430749a14a3a007d0e77b37923b6366aa1de
ivyroot <nonagoncup@gmail.com>
2022-10-03 15:30:52 -0700

    mockup basic website Coins Available Right Now

--- fullstack-web3/project/frontend/src/App.tsx
+++ fullstack-web3/project/frontend/src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex">
+        <img src={logo} className="w-32" alt="logo" />
+      </div>
+      <p className='m-8'>
+        Coins Available RN
+      </p>
+    </>
   );
 }
 
