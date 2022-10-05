1dcb410f360f88288db313bc141a2529edb8ba39
ivyroot <nonagoncup@gmail.com>
2022-10-04 22:02:34 -0700

    filter and reduce response to only the cheapest exchange

--- fullstack-web3/project/backend/src/index.js
+++ fullstack-web3/project/backend/src/index.js
@@ -22,26 +22,53 @@ const setCachedList = (coin, list) => {
     }
 }
 
-app.get('/:currency/:fiat', (req, res) => {
+const getCheapestExchange = (fiat, list) => {
+    const filtered = list.filter(
+        exchange => {
+            const pair = exchange.pair.split('/')
+            if (pair.length !== 2) {
+                return false;
+            }
+            return pair[1].toLowerCase() === fiat.toLowerCase()
+        }
+    );
+    if (filtered.length === 0) {
+        return null;
+    }
+    const cheapest = filtered.reduce(
+        (acc, exchange) => {
+            if (exchange.pairPrice < acc.pairPrice) {
+                return exchange;
+            }
+            return acc;
+        },
+        filtered[0]
+    );
+    return cheapest;
+}
+
+app.get('/cheapest-exchange/:currency/:fiat', (req, res) => {
     const { currency, fiat } = req.params;
     const cachedList = getCachedList(currency);
     if (cachedList) {
         console.log(`returning markets for ${currency} from cache`);
-        res.json({status: 'success', data: {markets: cachedList}});
+        const cheapestExchange = getCheapestExchange(fiat, cachedList);
+        res.json({status: 'success', data: cheapestExchange});
         return;
     }
     console.log(`fetching markets for ${currency} from coinstats API`);
     axios.get('/markets', {params: {coinId: currency}})
-        .then(function (response) {
-            setCachedList(currency, response.data);
-            res.json({status: 'success', data: {markets: response.data}});
-        })
-        .catch(function (error) {
-            console.log(error);
-            res.status(400);
-            res.json({status: 'error', message: error});
-        })
-    });
+    .then(function (response) {
+        setCachedList(currency, response.data);
+        const cheapestExchange = getCheapestExchange(fiat, response.data);
+        res.json({status: 'success', data: cheapestExchange});
+    })
+    .catch(function (error) {
+        console.log(error);
+        res.status(400);
+        res.json({status: 'error', message: error});
+    })
+});
 
 
 app.listen(port, () => {
